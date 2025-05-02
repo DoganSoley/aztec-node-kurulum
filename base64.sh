1d5a1399a236e83a8c820026542e04fa82845347
Doğan Soley <110679236+DoganSoley@users.noreply.github.com>
2025-05-02 08:54:59 +0300

    Update base64.sh

--- base64.sh
+++ base64.sh
@@ -5,7 +5,7 @@ BLOCK_NUM=$(curl -s -X POST -H 'Content-Type: application/json' \
   -d '{"jsonrpc":"2.0","method":"node_getL2Tips","params":[],"id":67}' \
   http://localhost:8080 | jq -r ".result.proven.number")
 
-echo "🔢 Son proven blok numarası: $BLOCK_NUM"
+echo "🔢 Son blok numarası: $BLOCK_NUM"
 
 # 2. Aynı blok numarasını kullanarak base64 proof al
 PROOF=$(curl -s -X POST -H 'Content-Type: application/json' \
