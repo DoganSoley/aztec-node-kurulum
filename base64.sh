42710c51576700c42f5f4f54634788dec54e811e
Doğan Soley <110679236+DoganSoley@users.noreply.github.com>
2025-05-02 07:57:55 +0300

    Create base64.sh

---
+++ base64.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 1. Proven block numarasını al
+BLOCK_NUM=$(curl -s -X POST -H 'Content-Type: application/json' \
+  -d '{"jsonrpc":"2.0","method":"node_getL2Tips","params":[],"id":67}' \
+  http://localhost:8080 | jq -r ".result.proven.number")
+
+echo "🔢 Son proven blok numarası: $BLOCK_NUM"
+
+# 2. Aynı blok numarasını kullanarak base64 proof al
+PROOF=$(curl -s -X POST -H 'Content-Type: application/json' \
+  -d "{\"jsonrpc\":\"2.0\",\"method\":\"node_getArchiveSiblingPath\",\"params\":[\"$BLOCK_NUM\",\"$BLOCK_NUM\"],\"id\":67}" \
+  http://localhost:8080 | jq -r ".result")
+
+echo -e "\n📦 Base64 Proof:\n$PROOF"
